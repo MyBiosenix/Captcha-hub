00654781af7d0f375c1bac46ac3f760358f7bc33
MyBiosenix <mymailbox8162024@gmail.com>
2025-11-22 13:58:27 +0530

    deployed backend

--- captcha-project/src/Admin/Components/UserFormComp.jsx
+++ captcha-project/src/Admin/Components/UserFormComp.jsx
@@ -107,7 +107,7 @@ function UserFormComp() {
         );
         alert('User Updated Successfully');
       } else {
-        await axios.post('http://localhost:5035/api/auth/user/create-user', {
+        await axios.post('https://api.captcha-google.com/api/auth/user/create-user', {
           name,
           email,
           mobile,
